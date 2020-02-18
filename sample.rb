06448b740d4fab0d75eeb85cb3dcbc5274e4e56f
micchan <p1r2yy9knr@gmail.com>
2020-02-18 14:08:51 +0900

    余計なものを削除

--- sample.rb
+++ sample.rb
@@ -8,8 +8,5 @@ module Clockwork
     client.notify(job)
   end
 
-  every(10.seconds, '10秒たったよ')
   every(3.minutes, '3分たったよ')
-  every(1.hour, '1時間たったよ')
-  every(1.day, '1日たったよ', :at => '00:00')
 end
